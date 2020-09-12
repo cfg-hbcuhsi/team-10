9e8d293d39333c79089d9a02a3cb7e4edacfe2ac
jmasmith <joshua.smith01@utrgv.edu>
2020-09-12 08:52:36 -0500

    collegeCards

--- src/components/College Screen/College.js
+++ src/components/College Screen/College.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { MDBContainer, MDBRow, MDBBtn, MDBCol, MDBIcon } from 'mdbreact';
+import { MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText } from 'mdbreact';
 import './College.css';
 
 class College extends Component {
@@ -32,7 +33,76 @@ class College extends Component {
                     {/* Filter options */}
                 </MDBRow>
                 <MDBRow>
-                    {/* Cards with info */}
+                    <MDBCol>
+                      <MDBCard className="widthLim" style={{ height: "10vh" }}>
+        
+                        <MDBCardBody>
+                          <MDBCardTitle>Computer Science</MDBCardTitle>
+                          <MDBCardText>
+                          For tech lovers. Learn what makes computers go and how they can be used to better society.
+                          </MDBCardText>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol>
+                    <MDBCol>
+                      <MDBCard className="widthLim" style={{ height: "50vh" }}>
+      
+                        <MDBCardBody>
+                          <MDBCardTitle>Physics</MDBCardTitle>
+                          <MDBCardText>
+                          Learn the math of the world, and how you can make a living with it.
+                          </MDBCardText>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol>
+                    <MDBCol>
+                      <MDBCard className="widthLim" style={{ height: "50vh" }}>
+      
+                        <MDBCardBody>
+                          <MDBCardTitle>Music Education</MDBCardTitle>
+                          <MDBCardText>
+                          Passionate about music? Learn to share that passion with generations of students as you prepare to be a music teacher.
+                          </MDBCardText>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol>
+                    <MDBCard className="widthLim" style={{ height: "50vh" }}>
+      
+                      <MDBCardBody>
+                        <MDBCardTitle>Chemistry</MDBCardTitle>
+                        <MDBCardText>
+                        A field where you learn of the various types of matter, and why it matters.
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                  <MDBCol>
+                  
+                    <MDBCard className="widthLim" style={{ height: "50vh" }}>
+                    
+                      <MDBCardBody>
+                        <MDBCardTitle>Liberal Arts</MDBCardTitle>
+                        <MDBCardText>
+                        I got nothin
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+    
+                  </MDBCol>
+                  <MDBCol>
+                  <MDBCard className="widthLim" style={{ height: "50vh" }}>
+      
+                      <MDBCardBody>
+                        <MDBCardTitle>English</MDBCardTitle>
+                        <MDBCardText>
+                        Reading, writing, and rhetoric through the ages.
+                        </MDBCardText>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
                 </MDBRow>
             </MDBContainer>
         )
