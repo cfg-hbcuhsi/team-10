aeee4e37cc84ede4f7e0235d0dff395f44a22d20
Maria Cordero <maria.cordero6@upr.com>
2020-09-12 09:18:42 -0400

    Merging changes

--- src/components/App.js
+++ src/components/App.js
@@ -20,16 +20,6 @@ class App extends Component {
         return (
             <div>
               <NavBar/>
-              <div>
-<<<<<<< HEAD
-                <MDBBtn outline color="primary" type="submit"  onClick={() => history.push("/college")}>College</MDBBtn>
-                <MDBBtn outline color="primary" type="submit"  onClick={() => history.push("/alternative-careers")}>Alternative</MDBBtn>
-                <MDBBtn outline color="primary" type="submit" onClick={() => history.push("/self-education")}>Self Education</MDBBtn>
-=======
-                <MDBBtn outline color="primary" type="submit" onClick={() => history.push("/")}>Home</MDBBtn>
->>>>>>> 6e5806f797e54c165fff4653c4c20a670f60cd2f
-                <MDBBtn outline color="primary" type="submit" onClick={() => history.push("/about")}>About Us</MDBBtn>
-              </div>
              <Routes/>
             </div>
         );
