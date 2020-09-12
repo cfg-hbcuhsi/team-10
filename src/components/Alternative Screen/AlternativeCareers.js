9d7bc5ac2ef3e56d9c10e85f01688a0aee4e0b50
Maria Cordero <maria.cordero6@upr.com>
2020-09-12 07:32:05 -0400

    merged alt with college screens

--- src/components/Alternative Screen/AlternativeCareers.js
+++ src/components/Alternative Screen/AlternativeCareers.js
@@ -17,46 +17,46 @@ class AlternativeCareers extends Component {
 
     render() {
         return (
-            <div>
-                <MDBRow>
-      <MDBCol>
-      <MDBCard className="widthLim" style={{ height: "80vh" }}>
-      <MDBCardImage className="img-fluid" src="https://comps.canstockphoto.com/military-uniform-soldier-row-pictures_csp6357868.jpg" waves />
-        <MDBCardBody>
-          <MDBCardTitle>Military</MDBCardTitle>
-          <MDBCardText>
-            There's more to the military than just combat roles. Click the button to learn more about military branches, the ASVAB, and military career paths.
-          </MDBCardText>
-          <MDBBtn href="#">Show me more!</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-    <MDBCol>
-      <MDBCard className="widthLim" style={{ height: "80vh" }}>
-      <MDBCardImage className="img-fluid" src="https://ak.picdn.net/offset/photos/577e8efcba6f6bfc1a487732/medium/offset_391828.jpg" waves />
-        <MDBCardBody>
-          <MDBCardTitle>Technical Careers</MDBCardTitle>
-          <MDBCardText>
-            Trades will always be in demand. If you like working with your hands, click the button to learn more about tradeschools, certifications, and apprenticeships
-          </MDBCardText>
-          <MDBBtn href="#">Show me more!</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-    <MDBCol>
-      <MDBCard className="widthLim" style={{ height: "80vh" }}>
-      <MDBCardImage className="img-fluid" src="https://media.gettyimages.com/photos/barista-taking-order-from-customer-cafe-picture-id1060887054?s=612x612" waves />
-        <MDBCardBody>
-          <MDBCardTitle>Entry Level Jobs</MDBCardTitle>
-          <MDBCardText>
-          Even just starting off working in retail or food can get you developing life-long skills. Click the button to learn more about local job ops, interviews, and resume tips.
-          </MDBCardText>
-          <MDBBtn href="#">Show me more!</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-    </MDBRow>
-            </div>
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol>
+                <MDBCard className="widthLim" style={{ height: "80vh" }}>
+                <MDBCardImage className="img-fluid" src="https://comps.canstockphoto.com/military-uniform-soldier-row-pictures_csp6357868.jpg" waves />
+                  <MDBCardBody>
+                    <MDBCardTitle>Military</MDBCardTitle>
+                    <MDBCardText>
+                      There's more to the military than just combat roles. Click the button to learn more about military branches, the ASVAB, and military career paths.
+                    </MDBCardText>
+                    <MDBBtn href="#">Show me more!</MDBBtn>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+              <MDBCol>
+                <MDBCard className="widthLim" style={{ height: "80vh" }}>
+                <MDBCardImage className="img-fluid" src="https://ak.picdn.net/offset/photos/577e8efcba6f6bfc1a487732/medium/offset_391828.jpg" waves />
+                  <MDBCardBody>
+                    <MDBCardTitle>Technical Careers</MDBCardTitle>
+                    <MDBCardText>
+                      Trades will always be in demand. If you like working with your hands, click the button to learn more about tradeschools, certifications, and apprenticeships
+                    </MDBCardText>
+                    <MDBBtn href="#">Show me more!</MDBBtn>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+              <MDBCol>
+                <MDBCard className="widthLim" style={{ height: "80vh" }}>
+                <MDBCardImage className="img-fluid" src="https://media.gettyimages.com/photos/barista-taking-order-from-customer-cafe-picture-id1060887054?s=612x612" waves />
+                  <MDBCardBody>
+                    <MDBCardTitle>Entry Level Jobs</MDBCardTitle>
+                    <MDBCardText>
+                    Even just starting off working in retail or food can get you developing life-long skills. Click the button to learn more about local job ops, interviews, and resume tips.
+                    </MDBCardText>
+                    <MDBBtn href="#">Show me more!</MDBBtn>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            </MDBRow>
+            </MDBContainer>
         );
     }
 
